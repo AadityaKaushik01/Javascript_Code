e0950c7025cbd6e2a2b259a1826d9b8840dc2423
AadityaKaushik01 <146659791+AadityaKaushik01@users.noreply.github.com>
2024-06-08 08:17:54 +0000

    add container config and files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountID = 54256
+let accountEmail= "akbhat254@gmail.com"
+var accountPassword = "khulja sinsin"
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword]);
