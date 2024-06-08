24b28492a3a8049e2371db55de16163889d1557b
AadityaKaushik01 <146659791+AadityaKaushik01@users.noreply.github.com>
2024-06-08 11:09:00 +0000

    created new file datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3 ) // we are using node.js, not browser
+
+console.log(6 - 5);
+
+console.log("Khiiiii");
+// tc39 ecma js original notes
+
+let name = "Aadi"
+let age =18
+let isLoggedIn = false
+let state = null; // null
+let statee;
+console.log(statee)
+console.log(state)
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined => 
+// symbol => unique  
+
+// object 
+
+console.log(typeof "adi" ); // string
+console.log(typeof age );  // number
+console.log(typeof null );  // object
+console.log(typeof undefined ); // undefined
+console.log(typeof "age" ); // string
